c39ee139ef54e2cddec13d2299a493cd650be9fa
Javran Cheng <Javran.c@gmail.com>
2019-03-27 00:29:14 -0700

    fetch version info from all servers

--- server-state-tool/src/Main.hs
+++ server-state-tool/src/Main.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.IntMap.Strict as IM
+import qualified Data.Map.Strict as M
 import Data.Maybe
 import Data.List
 import Data.Char
@@ -11,6 +12,7 @@ import Network.HTTP.Client
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import Text.ParserCombinators.ReadP
 import Control.Monad
+import Data.Aeson
 
 serverInfoP :: ReadP (Int, String)
 serverInfoP =
@@ -50,14 +52,20 @@ serverAddrToIp raw = case readP_to_S ipP raw of
       eof
       pure [a,b,c,d]
 
-
 main :: IO ()
 main = do
   mgr <- newManager tlsManagerSettings
   let simpleReq url = do
         req <- parseUrlThrow url
         responseBody <$> httpLbs req mgr
+      fetchVersionData :: String -> IO (Maybe (M.Map String String))
+      fetchVersionData ipAddr = do
+        let resourceUri = "http://" <> ipAddr <> "/kcs2/version.json"
+        raw <- simpleReq  resourceUri
+        pure (decode' raw)
   x <- T.unpack . decodeUtf8 . BSL.toStrict
     <$> simpleReq "http://203.104.209.7/gadget_html5/js/kcs_const.js"
-  print ((IM.map serverAddrToIp $ parseServerInfo x) :: IM.IntMap String)
+  let sMaps = IM.map serverAddrToIp $ parseServerInfo x
+  forM_ sMaps $ \ip ->
+    fetchVersionData ip >>= print
   pure ()
